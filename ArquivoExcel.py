34e89f1d0c1e409caf89f2558de347977dacd1d0
Davison Queiroz <davisonvinicius@outlook.com.br>
2025-01-15 15:30:21 -0300

    metodos para manipular o arquivo diretamente

--- ArquivoExcel.py
+++ ArquivoExcel.py
@@ -14,3 +14,21 @@ class ArquivoExcel:
             self.file_path = None
             self.visibility = visibility
             self.filtered = filtered
+
+    
+        #manipulação direta do arquivo
+
+    def create_new_file(self, directory, file_name):
+        file_path = os.path.join(directory,file_name)
+        self.save_file(file_path)
+
+    def close_file(self):
+        self.book.close()
+    
+    def save_file(self,file_path):
+        self.book.save(file_path)
+    
+    def close_file_without_saving(self):
+        self.book.saved = True
+        self.close_file()
+    
